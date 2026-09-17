c80b198b28bf47576c37a38d690fd67d9f203c9a
s-shivanand <sharana.shivanand@gmail.com>
2026-09-17 16:24:59 +0200

    adding pytest

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -28,10 +28,23 @@ class Calculator:
 
     def multiply(self, a: float, b: float) -> float:
         """
-        Subtracts the second number from the first.
+        Multiplies two numbers.
 
-        :param a: The first number (minuend).
-        :param b: The second number (subtrahend).
-        :return: The difference between a and b.
+        :param a: The first number.
+        :param b: The second number.
+        :return: The product of a and b.
         """
         return a * b
+
+    def divide(self, a: float, b: float) -> float:
+        """
+        Divides the first number by the second.
+
+        :param a: The first number (dividend).
+        :param b: The second number (divisor).
+        :raises ValueError: If b is zero.
+        :return: The quotient of a and b.
+        """
+        if b == 0:
+            raise ValueError("Cannot divide by zero.")
+        return a / b
